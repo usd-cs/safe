4cf15ba93ac5d03283da5fa21506ac7832c58b2e
Sat Garcia <sat@sandiego.edu>
2021-06-16 16:27:37 -0700

    refactor database stuff outside of main file

--- safe/__init__.py
+++ safe/__init__.py
@@ -10,6 +10,7 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         #DATABASE=os.path.join(app.instance_path, 'safe.sqlite'),
         DATABASE_URI='sqlite:///safe.sqlite3',
+        DATABASE_VERBOSE=True
     )
 
     # try to make the instance folder
@@ -18,70 +19,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    #from . import db
-    #db.init_app(app)
+    from . import db
+    db_engine = db.init_db(app)
 
-    engine = create_engine(app.config['DATABASE_URI'], echo=True)
 
-    # if database is empty (i.e. hasn't been initialized), initialize it by
-    # creating the basic tables.
-    if inspect(engine).get_table_names() == []:
-        print("Initializing database!")
-        engine.execute("""
-                        CREATE TABLE instructor (
-                          instructor_id INTEGER NOT NULL PRIMARY KEY,
-                          username VARCHAR NOT NULL UNIQUE,
-                          password VARCHAR NOT NULL
-                        )
-                        """)
-        engine.execute("""
-                        CREATE TABLE section (
-                          section_id INTEGER NOT NULL PRIMARY KEY,
-                          instructor_id INTEGER REFERENCES instructor
-                        )
-                        """)
-        engine.execute("""
-                        CREATE TABLE student (
-                          student_id INTEGER NOT NULL PRIMARY KEY,
-                          username VARCHAR NOT NULL UNIQUE,
-                          password VARCHAR NOT NULL,
-                          section_id INTEGER REFERENCES section
-                        )
-                        """)
-        engine.execute("""
-                        CREATE TABLE psa (
-                          psa_id INTEGER NOT NULL PRIMARY KEY,
-                          title VARCHAR NOT NULL
-                        )
-                        """)
-        engine.execute("""
-                        CREATE TABLE team (
-                          team_id INTEGER NOT NULL PRIMARY KEY,
-                          team_num INTEGER NOT NULL,
-                          psa_id INTEGER REFERENCES psa,
-                          section_id INTEGER REFERENCES section,
-                          CONSTRAINT uc_teaminfo UNIQUE (team_num, psa_id, section_id)
-                        )
-                        """)
-        engine.execute("""
-                        CREATE TABLE source_file (
-                          source_file_id INTEGER NOT NULL PRIMARY KEY,
-                          psa_id INTEGER REFERENCES psa,
-                          filename VARCHAR NOT NULL
-                        )
-                        """)
-        engine.execute("""
-                        CREATE TABLE student_team (
-                          student_id INTEGER REFERENCES student,
-                          team_id INTEGER REFERENCES team
-                        )
-                        """)
-        print(inspect(engine).get_table_names())
-
-    # create tables
-    from . import db_models
-    db_models.Base.metadata.create_all(engine)
-    Session = sessionmaker(engine)
+    Session = sessionmaker(db_engine)
 
     @app.route('/')
     def root():
--- safe/db.py
+++ safe/db.py
@@ -3,31 +3,79 @@ import sqlite3
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+from sqlalchemy import create_engine, inspect
 
 
 def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+    if 'db_engine' not in g:
+        g.db_engine = create_engine(app.config['DATABASE_URI'],
+                                    echo=app.config['DATABASE_VERBOSE'])
 
-    return g.db
+    return g.db_engine
 
 
-def close_db(e=None):
-    db = g.pop('db', None)
+def init_db(app):
+    engine = create_engine(app.config['DATABASE_URI'], echo=True)
 
-    if db is not None:
-        db.close()
+    # if database is empty (i.e. hasn't been initialized), initialize it by
+    # creating the basic tables.
+    if inspect(engine).get_table_names() == []:
+        print("Initializing database!")
 
+        # FIXME: use "with engine.connect() as connection" here for executing
+        engine.execute("""
+                        CREATE TABLE instructor (
+                          instructor_id INTEGER NOT NULL PRIMARY KEY,
+                          username VARCHAR NOT NULL UNIQUE,
+                          password VARCHAR NOT NULL
+                        )
+                        """)
+        engine.execute("""
+                        CREATE TABLE section (
+                          section_id INTEGER NOT NULL PRIMARY KEY,
+                          instructor_id INTEGER REFERENCES instructor
+                        )
+                        """)
+        engine.execute("""
+                        CREATE TABLE student (
+                          student_id INTEGER NOT NULL PRIMARY KEY,
+                          username VARCHAR NOT NULL UNIQUE,
+                          password VARCHAR NOT NULL,
+                          section_id INTEGER REFERENCES section
+                        )
+                        """)
+        engine.execute("""
+                        CREATE TABLE psa (
+                          psa_id INTEGER NOT NULL PRIMARY KEY,
+                          title VARCHAR NOT NULL
+                        )
+                        """)
+        engine.execute("""
+                        CREATE TABLE team (
+                          team_id INTEGER NOT NULL PRIMARY KEY,
+                          team_num INTEGER NOT NULL,
+                          psa_id INTEGER REFERENCES psa,
+                          section_id INTEGER REFERENCES section,
+                          CONSTRAINT uc_teaminfo UNIQUE (team_num, psa_id, section_id)
+                        )
+                        """)
+        engine.execute("""
+                        CREATE TABLE source_file (
+                          source_file_id INTEGER NOT NULL PRIMARY KEY,
+                          psa_id INTEGER REFERENCES psa,
+                          filename VARCHAR NOT NULL
+                        )
+                        """)
+        engine.execute("""
+                        CREATE TABLE student_team (
+                          student_id INTEGER REFERENCES student,
+                          team_id INTEGER REFERENCES team
+                        )
+                        """)
 
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    from . import db_models
+    db_models.Base.metadata.create_all(engine)
+    return engine
 
 
 @click.command('init-db')
