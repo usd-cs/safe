f7b5ef977a6f48ffd5f2542c6c1da649c5608500
Sat Garcia <sat@sandiego.edu>
2021-07-09 11:47:17 -0700

    default config has email enabled. move all email specific config to instance config file

--- config.py
+++ config.py
@@ -3,5 +3,4 @@ DATABASE_URI = 'sqlite:///:memory:'
 DATABASE_VERBOSE = True
 MAX_PASSWORD_RESET_TIME = 15
 SERVER_BASE_URL = 'http://localhost:5000'
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587
+EMAIL_ENABLED = False
