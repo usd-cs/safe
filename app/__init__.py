72c7c04b9e1e607ebda8e556b98b16112997b83f
Sat Garcia <sat@sandiego.edu>
2021-07-09 11:45:03 -0700

    slight update to how configs are loaded

--- app/__init__.py
+++ app/__init__.py
@@ -26,12 +26,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object('config')
-    app.config.from_pyfile('config.py')
-
-    app.config.from_mapping(
-        #DATABASE=os.path.join(app.instance_path, 'safe.sqlite'),
-        DATABASE_URI='sqlite:///safe.sqlite3',
-    )
+    app.config.from_pyfile('config.py', silent=True)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
