a9fff351ade6f8b92f9695981dbb172de1e4a6fa
Sat Garcia <sat@sandiego.edu>
2021-07-28 12:01:10 -0700

    switch from subprocess.call to GitPython module for repository cloning and pulling

--- app/workers.py
+++ app/workers.py
@@ -41,31 +41,27 @@ def run_test(git_server, repo_base_dir, repo_name, test_code_dir, test_command, 
 
     repo_location = os.path.join(repo_base_dir, repo_name)
 
-    # check if we need to clone the repository first
-    needs_pulled = True
-    if not os.path.exists(repo_location):
-        print("\tExisting repository not found. Cloning...")
-        os.chdir(f"{repo_base_dir}")
-        result = subprocess.call(["git", "clone", f"git@{git_server}:{repo_name}"])
-
-        if result != 0:
-            raise RuntimeError(f"Could not clone: {repo_name}")
-
-        needs_pulled = False # fresh clone so don't need to pull later
-
-    os.chdir(repo_location)
-
-    # Pull latest changes, if necessary
-    if needs_pulled:
-        print("\tPulling from repository...")
-        if subprocess.call(["git", "pull"]) != 0:
-            raise RuntimeError(f"Could not pull: {repo_name}")
-
     # import here to avoid making safe app container image require git
-    import git
+    from git import Repo
+
+    # check if we need to clone the repository first
+    if os.path.exists(repo_location):
+        # repo directory already exists, so just do a pull
+        print("\tExisting repository found. Pulling...")
+
+        os.chdir(repo_location)
+        repo = Repo(os.getcwd())
+        origin = repo.remote('origin')
+        origin.pull()
+
+    else:
+        # repo dir doesn't exist, so clone repository
+        print("\tExisting repository not found. Cloning...")
+
+        repo = Repo.clone_from(f"git@{git_server}:{repo_name}", repo_location)
+        os.chdir(repo_location)
 
     # get time, author, and message for latest commit
-    repo = git.Repo(os.getcwd())
     latest_commit = repo.commit('master')
 
     submission_time = str(latest_commit.committed_datetime)
