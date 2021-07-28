1e10d3d2d440028b5952fabee6c420b91d49627b
Sat Garcia <sat@sandiego.edu>
2021-07-28 15:40:02 -0700

    order an assignment's teams by their team number

--- app/db_models.py
+++ app/db_models.py
@@ -91,7 +91,7 @@ class Assignment(Base):
     base_assignment_id = Column(Integer, ForeignKey("base_assignment.assignment_id"))
 
     # one assignment can have many teams
-    teams = relationship("Team", backref=backref("assignment"))
+    teams = relationship("Team", order_by="Team.team_num", backref=backref("assignment"))
 
 
 class Team(Base):
