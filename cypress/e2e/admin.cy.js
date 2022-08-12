895d675e9a648ae392cc863cbb4bd24a5a6ac0ae
Sat Garcia <sat@sandiego.edu>
2022-08-12 12:13:34 -0700

    remove dead code

--- cypress/e2e/admin.cy.js
+++ cypress/e2e/admin.cy.js
@@ -43,49 +43,6 @@ describe('Administrative Actions', function() {
       cy.get('table').contains("Smarty")
       cy.get('table').contains("Pants")
       cy.get('table').contains("spants")
-
-
-      /*
-      cy.get('input[name=title]').type("Intro to Narwhals")
-      cy.get('textarea[name=description]').type("A very interesting course")
-      cy.get('input[name=start_date]').type('2021-01-03')
-      cy.get('input[name=end_date]').type('2095-07-13')
-
-      // TODO: check URL to make sure we are at setup textbooks
-      cy.get('input[id=textbookSearchBar]').type(`${this.textbook1.title}{enter}`)
-      cy.contains('button', 'Add').click()
-      cy.contains("Continue...").click()
-      cy.contains("Next Step").click()
-
-      // TODO: check url to make sure we are at setup topics
-      cy.get('[data-cy=topicList]').as('sectionTopics')
-
-      // Add all topics from first section and one from the third section
-      cy.get('@sectionTopics').first().contains("Add All").click()
-      cy.get('[data-cy=topicsPage]').children().should('have.length', 3)
-
-      cy.contains(this.textbook1.sections[2].topics[1].text).click()
-      cy.get('[data-cy=topicsPage]').children().should('have.length', 4)
-
-      // remove one of the topics
-      cy.get('[data-cy=topicsPage]').children().eq(1).contains("Remove").click()
-
-      cy.contains("Search / Create").click()
-
-      // search for non-existent topic and add it through here
-      cy.get('#topicSearchBar').type("aaabbb{enter}")
-      cy.contains("Create and Add Topic").click()
-      cy.get("#courseTopics").should('contain', 'aaabbb')
-
-      cy.get('#topicSearchBar').type(`{selectAll}{backspace}${this.extraTopics[0].text}{enter}`)
-      cy.get('[data-cy=result0]').should('contain', this.extraTopics[0].text).find('button').click()
-
-      cy.get('#courseTopics').should('contain', this.extraTopics[0].text)
-      cy.contains("Continue...").click()
-      cy.contains("Exit Course Setup").click()
-
-      cy.location('pathname').should('eq', `/c/test-course1`)
-      */
     })
 
     it('Create New Section', function () {
